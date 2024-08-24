126526dea6690d112ea52f77274c41e334b6fdb3
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-24 08:58:56 +0500

    bar chart (top-right)

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 
@@ -15,12 +16,14 @@ const AdminPage = () => {
         </div>
         {/* middle chart */}
         <div className="flex gap-4 flex-col lg:flex-row ">
-            {/* Count chart */}
-            <div className="w-full lg:w-1/3 h-[450px]">
+          {/* Count chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
             <CountChart />
-            </div>
-            {/* attendance chart */}
-            <div className="w-full lg:w-2/3 h-[450px]"></div>
+          </div>
+          {/* attendance chart */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+          <AttendanceChart />
+          </div>
         </div>
         {/* bottom chart */}
         <div className=""></div>
---
+++ src/components/AttendanceChart.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import Image from "next/image";
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "Mon",
+    present: 90,
+    absent: 25,
+  },
+  {
+    name: "Tue",
+    present: 100,
+    absent: 70,
+  },
+  {
+    name: "Wen",
+    present: 123,
+    absent: 55,
+  },
+  {
+    name: "Thu",
+    present: 124,
+    absent: 65,
+  },
+  {
+    name: "Fri",
+    present: 155,
+    absent: 43,
+  },
+];
+
+const AttendanceChart = () => {
+  return (
+    <div className="bg-white rounded-lg p-4 h-full w-full">
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Attendace</h1>
+        <Image src={"/moreDark.png"} alt="" width={20} height={20} />
+      </div>
+      <ResponsiveContainer width="100%" height="90%">
+        <BarChart width={500} height={300} data={data} barSize={20}>
+          <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd"/>
+          <XAxis dataKey="name"  axisLine={false} tick={{fill:"#d1d5db"}} tickLine={false} />
+          <YAxis axisLine={false}  tickLine={false}/>
+          <Tooltip contentStyle={{borderRadius:"10px",borderColor:"lightgray"}} />
+          <Legend
+            align="left"
+            verticalAlign="top"
+            wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }}
+          />
+          <Bar
+            dataKey="present"
+            fill="#FAE27C"
+            activeBar={<Rectangle fill="pink" stroke="blue" />}
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
+          <Bar
+            dataKey="absent"
+            fill="#C3EBFA"
+            activeBar={<Rectangle fill="gold" stroke="purple" />}
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default AttendanceChart;
