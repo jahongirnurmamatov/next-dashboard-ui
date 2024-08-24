482db138c080e59245d58430d37085c1b3817fc7
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-08-24 17:10:31 +0500

    Table component responsive

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,8 +1,99 @@
 import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
+import { role, teachersData } from "@/lib/data";
 import Image from "next/image";
+import Link from "next/link";
 
+type Teacher = {
+  id: number;
+  teacherId: string;
+  name: string;
+  email?: string;
+  photo: string;
+  phone: string;
+  subjects: string[];
+  classes: string[];
+  address: string;
+};
+
+const columns = [
+  {
+    header: "Info",
+    accessor: "info",
+  },
+  {
+    header: "Teacher ID",
+    accessor: "teacherId",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Subjects",
+    accessor: "suubjects",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Classes",
+    accessor: "classes",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Address",
+    accessor: "address",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "actions",
+  },
+];
 const TeacherList = () => {
+  const renderRow = (item: Teacher) => {
+    return (
+      <tr
+        key={item.id}
+        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurple"
+      >
+        <td className="flex gap-2 items-center p-4">
+          <Image
+            src={item.photo}
+            alt=""
+            width={40}
+            height={40}
+            className="rounded-full md:hidden xl:block w-10 h-10 object-cover"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-semibold">{item.name}</h3>
+            <p className="text-sm text-gray-400">{item?.email}</p>
+          </div>
+        </td>
+        <td className="hidden md:table-cell">{item.teacherId}</td>
+        <td className="hidden md:table-cell">{item.subjects.join(", ")}</td>
+        <td className="hidden md:table-cell">{item.classes.join(", ")}</td>
+        <td className="hidden md:table-cell">{item.phone}</td>
+        <td className="hidden md:table-cell">{item.address}</td>
+        <td>
+          <div className="flex items-center gap-2">
+            <Link href={`/list/teachers/${item.id}`}>
+              <button className="h-7 w-7 flex items-center justify-center rounded-full bg-lamaSky">
+                <Image alt="" src={"/view.png"} width={16} height={16} />
+              </button>
+            </Link>
+            {role === "admin" && (
+              <button className="h-7 w-7 flex items-center justify-center rounded-full bg-lamaPurple">
+                <Image alt="" src={"/delete.png"} width={16} height={16} />
+              </button>
+            )}
+          </div>
+        </td>
+      </tr>
+    );
+  };
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* top */}
@@ -24,7 +115,7 @@ const TeacherList = () => {
         </div>
       </div>
       {/* List */}
-      <div className=""></div>
+      <Table columns={columns} renderRow={renderRow} data={teachersData} />
       {/* pagination */}
       <Pagination />
     </div>
